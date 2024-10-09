f357a2d554d4b2779ec7f3173fe21193ee9f6ba3
AndreFernandoM <andrefernando777@hotmail.com>
2024-10-09 19:34:34 -0300

    feat: UI de notas

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,47 +1,69 @@
 import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView
+} from "react-native";
+import { Chip } from "react-native-paper";
 
-import { ScrollView, View, StyleSheet, TextInput } from "react-native";
-import { Text, RadioButton } from "react-native-paper";
+const mock = {
+  color: "#fffbe0"
+};
 
 const Note = () => {
-  const [value, setValue] = useState("first");
-  const [text, setText] = useState("");
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [text, setText] = useState(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  );
+  const [title, setTitle] = useState("Homework");
 
-  const mock = {
-    title: "AAAAAAAAAAAAAA",
-    content: "BBBBBBBBBBBBBB",
-
-    color: "#32CD32",
-    /*cor aleatoria?*/
-
-    pet: {
-      gato: "Gato01",
-      cachorro: "Cachorro01"
-    }
-  };
+  const [dogChecked, setDogChecked] = useState(false);
+  const [catChecked, setCatChecked] = useState(false);
 
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <Text>{mock.title}</Text>
-        <Text>{mock.content}</Text>
-        <RadioButton.Group
-          onValueChange={(value) => setValue(value)}
-          value={value}
-        >
-          <RadioButton.Item label={mock.pet.gato} value="first" />
-          <RadioButton.Item label={mock.pet.cachorro} value="second" />
-        </RadioButton.Group>
+      <KeyboardAvoidingView behavior="padding" style={styles.contentContainer}>
+        <View style={styles.selectContainer}>
+          <Chip
+            icon="cat"
+            style={[styles.chip, catChecked ? styles.selectedChip : {}]}
+            textStyle={catChecked ? {} : styles.unselectedText}
+            onPress={() => setCatChecked(!catChecked)}
+          >
+            Gato
+          </Chip>
+          <Chip
+            icon="dog"
+            style={[styles.chip, dogChecked ? styles.selectedChip : {}]}
+            textStyle={dogChecked ? {} : styles.unselectedText}
+            onPress={() => setDogChecked(!dogChecked)}
+          >
+            Cachorro
+          </Chip>
+        </View>
 
-        <TextInput
-          placeholder="Anotação sobre seu pet"
-          value={text}
-          onChangeText={(text) => setText(text)}
-          style={styles.cardContent}
-        />
-      </ScrollView>
+        {/* Seção de Nota */}
+        <ScrollView>
+          <View style={styles.noteContainer}>
+            <TextInput
+              style={styles.title}
+              editable={true}
+              value={title}
+              onChangeText={setTitle}
+            />
+            <TextInput
+              value={text}
+              onChangeText={setText}
+              multiline={true}
+              style={styles.textInput}
+              placeholder="Escreva sua nota aqui..."
+              autoFocus={false}
+              editable={true}
+            />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
@@ -51,16 +73,51 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  scrollContainer: {
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    justifyContent: "center"
+  contentContainer: {
+    flex: 1,
+    paddingHorizontal: 20
   },
-  cardContent: {
-    backgroundColor: "#d8d8d8",
-    // minHeight: "30%", isso aqui tem q pegar a tela que sobrar
-    margin: 0,
-    padding: 5
+
+  noteContainer: {
+    // backgroundColor: "#fffbe0", // OU SETA TUDO DESSA COR AQUI MESMO E JA ERA
+    backgroundColor: mock.color, // Cor de fundo MESMA DA COR NA HOME
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+    elevation: 1
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10
+  },
+  textInput: {
+    // backgroundColor: "#fffbe0", // OU SETA TUDO DESSA COR AQUI MESMO E JA ERA
+    backgroundColor: mock.color, // Cor de fundo MESMA DA COR NA HOME
+    fontSize: 16,
+    color: "#333",
+    textAlignVertical: "top",
+    minHeight: 200,
+    borderWidth: 0
+  },
+
+  selectContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 20
+  },
+  chip: {
+    marginHorizontal: 10,
+    borderRadius: 20,
+    backgroundColor: "#f0f0f0"
+  },
+  selectedChip: {
+    backgroundColor: "#ebdefa"
+  },
+  unselectedText: {
+    color: "#999999"
   }
 });
+
 export default Note;
