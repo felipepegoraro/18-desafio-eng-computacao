6ca9676501bdea149c4d0af75ed8670fa445d489
felipe pegoraro <fesousapegora03@gmail.com>
2024-10-09 22:49:53 -0300

    add: notas/falta coisas :)

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import { auth } from "../firebaseConfig";//db
+import type { Note } from "../firestore/modelNotes";
+import { createNote } from "../firestore/modelNotes";
+import { useState, useEffect } from "react";
+import { Picker } from '@react-native-picker/picker';
+import { getUserNotes, getUserPets } from '../firestore/createUsers';
+import type { Pet } from '../firestore/createPets';
+
 import {
   ScrollView,
   View,
   StyleSheet,
   TextInput,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Button
 } from "react-native";
 import { Chip } from "react-native-paper";
 
@@ -12,18 +20,63 @@ const mock = {
   color: "#fffbe0"
 };
 
-const Note = () => {
-  const [text, setText] = useState(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  );
-  const [title, setTitle] = useState("Homework");
+// usuario possui nota?
+    // se sim exibir as notas
+    // se nao, tela "Adicione novas notas"
+// cada nota: 
+    // editavel
+    // marcada como completada
+    // deletavel
+// (talvez: possibilidade de selecionar e (deletar | concluir))
+// botao de criar novas notas
 
-  const [dogChecked, setDogChecked] = useState(false);
-  const [catChecked, setCatChecked] = useState(false);
 
-  return (
-    <View style={styles.container}>
-      <KeyboardAvoidingView behavior="padding" style={styles.contentContainer}>
+
+
+const NoteUI = () => {
+    const user = auth.currentUser;
+    const [notes, setNotes] = useState<Note[]>([]);
+    const [pets, setPets] = useState<Pet[]>([]);
+    const [selectedPetId, setSelectedPetId] = useState<string>("");
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+
+    const [dogChecked, setDogChecked] = useState(false);
+    const [catChecked, setCatChecked] = useState(false);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            if (user) {
+                const userNotes = await getUserNotes(user.uid);
+                const userPets = await getUserPets(user.uid);
+                console.log(userPets)
+                if (userNotes) setNotes(userNotes);
+                if (userPets) setPets(userPets);
+                console.log(userNotes);
+            }
+        };
+
+        fetchUserData();
+    }, [user]);
+
+    const handleCreateNote = async () => {
+        if (title && text && selectedPetId) {
+            await createNote(user!.uid, selectedPetId, title, text, null);
+            setTitle("");
+            setText("");
+            setSelectedPetId("");
+
+            const userNotes = await getUserNotes(user!.uid);
+            if (userNotes) setNotes(userNotes);
+        } else {
+            alert("Por favor, preencha todos os campos!");
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAvoidingView behavior="padding" style={styles.contentContainer}>
+
         <View style={styles.selectContainer}>
           <Chip
             icon="cat"
@@ -42,30 +95,61 @@ const Note = () => {
             Cachorro
           </Chip>
         </View>
+            {/* Seção de Nota */}
+            {notes.length > 0 ? (
+                <ScrollView>
+                    {notes.map((note, index) => (
+                        <View key={index} style={styles.noteContainer}>
+                            <TextInput
+                                style={styles.title}
+                                editable={true}
+                                value={note.title}
+                                onChangeText={(text) => {}}
+                            />
+                            <TextInput
+                                value={note.content}
+                                onChangeText={(text) => {}}
+                                multiline={true}
+                                style={styles.textInput}
+                                placeholder="Escreva sua nota aqui..."
+                                autoFocus={false}
+                                editable={true}
+                            />
+                        </View>
+                    ))}
+                </ScrollView>
+            ) : ( <>
+                {/*adicionar texto: "CRIE SUA PRIMEIRA NOTA" ou algo do tipo, antes disso tudo: */}
+                <Picker
+                    selectedValue={selectedPetId}
+                    onValueChange={(itemValue: string) => setSelectedPetId(itemValue)}
+                >
+                    <Picker.Item label="Selecione um pet" value={""} />
+                    {pets.map((pet) => (
+                        <Picker.Item key={pet.id} label={pet.name} value={pet.id} />
+                    ))}
+                </Picker> 
 
-        {/* Seção de Nota */}
-        <ScrollView>
-          <View style={styles.noteContainer}>
-            <TextInput
-              style={styles.title}
-              editable={true}
-              value={title}
-              onChangeText={setTitle}
-            />
-            <TextInput
-              value={text}
-              onChangeText={setText}
-              multiline={true}
-              style={styles.textInput}
-              placeholder="Escreva sua nota aqui..."
-              autoFocus={false}
-              editable={true}
-            />
-          </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </View>
-  );
+                <View>
+                    <TextInput
+                        style={styles.title}
+                        placeholder="Título da nota"
+                        value={title}
+                        onChangeText={setTitle}
+                    />
+                    <TextInput
+                        value={text}
+                        onChangeText={setText}
+                        multiline={true}
+                        style={styles.textInput}
+                        placeholder="Escreva sua nota aqui..."
+                    />
+                    <Button title="Criar Nota" onPress={handleCreateNote} />
+                </View>
+            </>)}
+            </KeyboardAvoidingView>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -120,4 +204,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Note;
+export default NoteUI;
--- src/firestore/createUsers.tsx
+++ src/firestore/createUsers.tsx
@@ -1,5 +1,7 @@
-import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
+import type { Note } from './modelNotes';
+import type { Pet } from './createPets';
+import { doc, setDoc, collection, getDocs, query, where } from 'firebase/firestore';
 
 export interface User {
     name: string;
@@ -11,10 +13,71 @@ export const createUser = async (userId: string, name: string, email: string): P
     await setDoc(doc(db, 'users', userId), {
       name: name,
       email: email,
-      ownsPet: false
+      ownsPet: false,
     } as User);
     console.log('Usuário criado com sucesso!');
   } catch (error) {
     console.error('Erro ao criar usuário: ', error);
   }
 };
+
+
+export const getUserPets = async (userId: string): Promise<Pet[] | null> => {
+    try {
+        const petsRef = collection(db, "pets");
+        const q = query(petsRef, where("userId", "==", userId));
+        const querySnapshot = await getDocs(q);
+        
+        const pets: Pet[] = [];
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            pets.push({
+                id: doc.id,
+                userId: data.userId,
+                name: data.name,
+                type: data.type, // DOG or CAT
+                breed: data.breed,
+                gender: data.gender,
+                weight: data.weight,
+                birthDate: data.birthDate.toDate(),
+                notes: data.notes || "",
+            } as Pet);
+        });
+
+        console.log(pets);
+        return pets;
+    } catch (error) {
+        console.log("Erro ao buscar pets: ", error);
+    }
+
+    return null;
+};
+
+
+export const getUserNotes = async (userId: string): Promise<Note[] | null> => {
+    try {
+        const notesRef = collection(db, "notes");
+        const q = query(notesRef, where("userId", "==", userId));
+        const querySnapshot = await getDocs(q);
+        
+        const notes: Note[] = [];
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            notes.push({
+                title: data.title,
+                content: data.content,
+                petId: data.petId,
+                createdAt: data.createdAt.toDate(),
+                completedAt: data.completedAt ? data.completedAt.toDate() : null,
+                dueDate: data.dueDate ? data.dueDate.toDate() : null,
+            } as Note);
+            console.log(notes);
+        });
+
+        return notes;
+    } catch (error) {
+        console.log("Erro ao buscar notas: ", error);
+    }
+
+    return null;
+}
