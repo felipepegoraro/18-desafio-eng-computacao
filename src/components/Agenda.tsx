71689de32e422d0cf5e414d217e869aa313d55ab
felipe pegoraro <fesousapegora03@gmail.com>
2024-10-13 19:14:23 -0300

    calendario modal falta front end / ta bem feio

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -3,7 +3,10 @@ import { Calendar } from 'react-native-calendars';
 import { useState, useEffect } from 'react';
 import type { Note } from '../firestore/modelNotes';
 import { getUserNotes } from '../firestore/createUsers';
-import { View, Text } from "react-native";
+import { Dimensions, StyleSheet, Button, Modal, View, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+const { width } = Dimensions.get("window");
 
 interface MarkedDate {
     selected: boolean;
@@ -17,18 +20,23 @@ interface MarkedDate {
 }
 
 type MarkedDates = { [date: string]: MarkedDate };
-type emBreveType = {fiveDays: number, today: Note[]} | null;
+type emBreveType = { fiveDays: number, today: Note[] } | null;
+
+type ModalType = {
+    note: Note[],
+    visible: boolean,
+    position: { x: number, y: number }
+};
 
 const Agenda = () => {
-    console.log("render");
     const user = auth.currentUser;
     const [markedDates, setMarkedDates] = useState<MarkedDates>({});
-    const [selectedNote, setSelectedNote] = useState<Note | null>(null);
     const [emBreve, setEmBreve] = useState<emBreveType>(null);
+    const [modalClickedDay, setModalClicked] = useState<ModalType>({ note: [], visible: false, position: { x: 0, y: 0 } });
 
     useEffect(() => {
         if (user) calendarNotes(user.uid);
-    }, [user]);
+    }, [user])
 
     const formatDateCalendar = (date: Date | null): string => {
         if (!date) return "Data invalida";
@@ -50,18 +58,18 @@ const Agenda = () => {
         const diffDays = Math.ceil((noteDateMidnight.getTime() - todayMidnight.getTime()) / msd);
         const color = diffDays <= 5 ? "red" : diffDays <= 20 ? "#FFD700" : "green";
 
-        if (color === "red"){
+        if (color === "red") {
             setEmBreve((prev: emBreveType) => {
                 return (prev != null)
-                ? { fiveDays: prev.fiveDays+1, today: (diffDays === 0)
-                    ? [...prev.today, note] : [note]
-                } : {fiveDays: 1, today: diffDays === 0 ? [note] : []}
+                    ? { fiveDays: prev.fiveDays + 1, today: (diffDays === 0)
+                        ? [...prev.today, note] : [note]
+                    } : { fiveDays: 1, today: diffDays === 0 ? [note] : [] }
             })
         }
 
         return color;
     }
- 
+
     const calendarNotes = async (userId: string) => {
         const daysNotes = await getUserNotes(userId);
         if (daysNotes == null) return;
@@ -98,32 +106,75 @@ const Agenda = () => {
         setMarkedDates(datesMarked);
     };
 
-    const renderCalendar = () => (
-        <Calendar
-            onDayPress={(day: any) => {
-                const note = markedDates[day.dateString]?.note;
-                if (note) note.map(i => console.log("nota pra hoje: ", i.title))
-                setSelectedNote(note[1]);
-            }}
-            markedDates={markedDates}
-            markingType={'multi-dot'}
-        />
-    );
+    const handleDayClick = (day: any) => {
+        if (markedDates[day.dateString]) {
+            const notes = markedDates[day.dateString].note;
+            if (notes) setModalClicked({
+                note: notes,
+                visible: true,
+                position: { x: width * 0.32, y: 120 } // TODO: o certo era pegar o (x,y) do click MAS o CALENDAR nao deixa/nao consegui
+            })
+        }
+    }
 
+    const renderCalendar = () => {
+        return (
+            <View>
+                <Calendar
+                    onDayPress={handleDayClick}
+                    markedDates={markedDates}
+                    markingType={'multi-dot'}
+                    // onMonthChange={onMonthChange}
+                    hideArrows={false}
+                />
+            </View>
+        );
+    };
+
+    {/*TODO: front end daqui*/}
     return (
-        <View> 
+        <View>
             <Text>Compromissos: {
                 Object.values(markedDates).reduce((acc, markedDate) => {
                     return acc + markedDate.dots.length;
                 }, 0)
             }</Text>
+
             {emBreve && <Text>Em menos de 5 dias: {emBreve.fiveDays}</Text>}
             {emBreve && <Text>Para hoje: {emBreve.today ? emBreve.today.map(i => i.title).join(', ') : "nenhum compromisso"}</Text>}
+
             {renderCalendar()}
-            {/*TODO: card da nota, poder editar/apagar nota*/}
-            { selectedNote != null && <Text>{selectedNote.title}</Text> }
+
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalClickedDay?.visible}
+                onRequestClose={() => setModalClicked(prev => ({ ...prev, visible: false } as ModalType))}
+            >
+                <View style={[styles.modalView, { top: modalClickedDay?.position.y, left: modalClickedDay?.position.x }]}>
+                    <ScrollView>
+                        {modalClickedDay.note.length > 0 && modalClickedDay.note.map(n => (<Text key={n.id} style={styles.modalText}>{n.title}</Text>))}
+                    </ScrollView>
+                    <Button title="Fechar" onPress={() => setModalClicked(prev => ({ ...prev, visible: false } as ModalType))} />
+                </View>
+            </Modal>
         </View>
-        )
+    )
 };
 
 export default Agenda;
+
+const styles = StyleSheet.create({
+    modalView: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 20,
+        elevation: 5,
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+});
+
